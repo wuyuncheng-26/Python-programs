419a15649bf39aa22a9a81f4fdb15c68018171fa
wuyuncheng-26 <wujihuang7@hotmail.com>
2023-07-15 11:54:20 +0800

    修改程序”出数学题“

--- 出数学题.py
+++ 出数学题.py
@@ -1,12 +1,12 @@
 from random import randint
 from time import sleep
-n = int(input("""1：加法题
+n = input("""1：加法题
 2：减法题
 3：乘法题
 4：除法题
 
-请问您要生成什么题目："""))
-if n == 1:
+请问您要生成什么题目：""")
+if n == "1":
     a = randint(1000, 4999)
     b = randint(1000, 4999)
     print(f"{a}+{b}")
@@ -15,7 +15,7 @@ if n == 1:
     else:
         print("输入错误！")
         sleep(1)
-elif n == 2:
+elif n == "2":
     a = randint(1000, 4999)
     b = randint(1000, a - 500)
     print(f"{a}-{b}")
@@ -24,7 +24,7 @@ elif n == 2:
     else:
         print("输入错误！")
         sleep(1)
-elif n == 3:
+elif n == "3":
     a = randint(10, 99)
     b = randint(10, 99)
     print(f"{a}×{b}")
@@ -33,9 +33,9 @@ elif n == 3:
     else:
         print("输入错误！")
         sleep(1)
-elif n == 4:
-    a = randint(10000, 49999)
-    b = randint(10, 99)
+elif n == "4":
+    a = randint(10000, 24999)
+    b = randint(10, 49)
     print(f"{a}÷{b}")
     if input("输入“answer”查看答案：") == "answer":
         print(f"答案是：{a // b}……{a % b}")
