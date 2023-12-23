e0289fe13d78991a056ba273837e3d855d7739ef
Yuncheng Wu <122529705+wuyuncheng-26@users.noreply.github.com>
2023-12-23 09:50:08 +0800

    修改程序“信息轰炸“

--- 信息轰炸.py
+++ 信息轰炸.py
@@ -1,5 +1,8 @@
 from pynput.keyboard import Key, Controller as key_Controller
 import time
+print("请勿将本程序用于非法用途")
+time.sleep(1)
+print("5 秒后开始轰炸")
 time.sleep(5)
 keyboard = key_Controller()
 while True:
